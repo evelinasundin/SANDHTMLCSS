29e7dff483e077755b45a37c14695f704d01c8ca
Evelina Sundin <evelinasundin@Evelinas-MacBook-Pro-3.local>
2017-04-23 22:11:19 +0200

    all files added

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//JAVASCRIPT CODE
+
+/*MENU BAR*/
+
+document.getElementById('hamburgler').addEventListener('click', checkNav);
+    window.addEventListener("keyup", function(e) {
+   if (e.keyCode == 27) closeNav();
+ }, false);
+
+    function checkNav() {
+   if (document.body.classList.contains('hamburgler-active')) {
+     closeNav();
+   }
+ 
+   else {
+     openNav();
+   }
+ }
+
+// addEventListener(closeNav)
+
+    function closeNav() {
+   document.body.classList.remove('hamburgler-active');
+ }
+
+    function openNav() {
+   document.body.classList.add('hamburgler-active');
+ }
+
+    
+
+
+
+
